257ad7554653525f5146b6ff0d41d98887d4fa6e
thafisG <144493551+thafisG@users.noreply.github.com>
2023-12-26 21:48:33 -0300

    Create Pessoa.cs

---
+++ Models/Pessoa.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace EXEMPLO_PROPRIEDADE.Models
+{
+    public class Pessoa
+    {
+        public Pessoa()
+        {
+        }
+        public Pessoa(string nome, string sobrenome)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
+        private String _nome;
+        private int _idade;
+
+        public string Nome { 
+            get
+            {
+                return _nome.ToUpper();
+            } 
+
+            set
+            {
+                if(value == "")
+                {
+                    throw new ArgumentException("O nome não pode ser vazio");
+                }
+
+                _nome = value;
+            }
+        }
+
+        public string Sobrenome { get; set; }
+        public string NomeCompleto => $"{Nome} {Sobrenome}";
+        public int Idade 
+        { 
+            get => _idade;
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("A idade não pode ser menor que zero");
+
+                    _idade = value;
+                }
+            }
+        }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome: {Nome} {Sobrenome}, Idade: {Idade}");
+        }
+    }
+}
